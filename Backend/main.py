f0f5a6fd8c6843f38e8065898e0139baa15305ba
Justin Flick <justinjayflick@gmail.com>
2018-07-21 17:32:01 -0700

    ugh

--- Backend/main.py
+++ Backend/main.py
@@ -21,47 +21,51 @@ def create_user(user_json):
 
     cursor = connect_to_db()
 
-    try:
 
-        new_user = json.loads(user_json)
-        user = new_user["user"]
-        skills = new_user["skills"]
-        interests = new_user["interests"]
-        pic = new_user["profilepic"]
 
-        userID = random.randint(100000,999999)
+    new_user = user_json
+    user = new_user["User"]
+    skills = new_user["Skills"]
+    interests = new_user["Interests"]
+    pic = new_user["profilepic"]
 
-        cursor.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s,%s,%s,%s) ", (userID, user["Name"], user["email"], user["phoneNumber"], user["city"], user["state"], user["userType"], user["Bio"]))
+    userID = random.randint(100000,999999)
 
-        for skill in skills:
-            cursor.execute("INSERT INTO userSkills values (%s,%s)", userID, skill)
+    cursor.execute("INSERT INTO users VALUES (%s, %s, %s, %s, %s,%s,%s,%s) ", (userID, user["Name"], user["email"], user["phoneNumber"], user["city"], user["state"], user["userType"], user["Bio"]))
 
-        for interest in interests:
-            cursor.execute("INSERT INTO userInterests values (%s, %s)", userID, interest)
+    for skill in skills:
+        cursor.execute("INSERT INTO userSkills values (%s,%s)", (userID, skill))
 
-        picID = random.randint(100000,999999)
+    for interest in interests:
+        cursor.execute("INSERT INTO userInterest values (%s, %s)", (userID, interest))
 
-        cursor.execute("INSERT INTO userInterests values (%s, %s, %s)", picID, pic, userID)
+    picID = random.randint(100000,999999)
 
-        return userID
+    cursor.execute("INSERT INTO userProfilePicture values (%s, %s, %s)", (picID, pic, userID))
 
-    except:
-        return "Error: Unable to successfully create user"
+    return userID
+
+
 
 def get_user(user_to_get):
 
     cursor = connect_to_db()
     cursor.execute("SELECT * from users where userID like %s", user_to_get["userID"])
-    user_info = cursor.readlines()
+    user_info = cursor.fetchone()
+    print(user_info)
 
-    cursor.execute('SELECT skill from userskills where users_userID like %s', user_to_get["userID"])
-    skills = cursor.readlines()
+    cursor.execute('SELECT skill from userSkills where users_userID like %s', user_to_get["userID"])
+    skills = cursor.fetchall()
 
-    cursor.execute('SELECT skill from userinterests where users_userID like %s', user_to_get["userID"])
-    interests = cursor.readlines()
+    cursor.execute('SELECT interest from userInterest where users_userID like %s', user_to_get["userID"])
+    interests = cursor.fetchall()
 
     cursor.execute('Select pictureLocation from userProfilePicture where users_userID like %s', user_to_get["userID"])
-    pic = cursor.readline()
+    pic = cursor.fetchone()
+
+    if pic == "":
+        pic = "https://s3-us-west-2.amazonaws.com/stkhacakthon/default-profile.png"
+
 
 
 
@@ -70,7 +74,7 @@ def get_user(user_to_get):
         "email": user_info[2],
         "phoneNumber": user_info[3],
         "city": user_info[4],
-        "state": user_info[5]
+        "state": user_info[5],
         "userType": user_info[6],
         "Bio": user_info[7]
     }
--- Backend/routes.py
+++ Backend/routes.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/api/v1.0/create_user', methods=["POST"])
 def create_user_route():
-    if request.method == 'POST':
+    if request.method == 'POST' or request.method == "OPTIONS":
         newuser = request.json
         user_status = create_user(newuser)
         if "Error" in user_status:
@@ -29,19 +29,11 @@ def retrive_user_route():
         user = get_user(user_to_get)
         return jsonify(user)
 
+@app.route("/", methods=["GET"])
+def test():
+    return("sucess")
 
 
 if __name__ == '__main__':
-    # new_members = get_new_member_fitness_data()
-    # for row in new_members:
-    #     age = row[4]
-    #     gender = row[3]
-    #     member_id = row[0]
-    #     cal_consumed = row[8]
-    #     cal_burned = row[9]
-    #     print(row)
-    #     min_sleep, max_sleep = get_recommended_sleep(age)
-    #     sedentary, moderate, active = get_reccommended_calories(gender, age)
-    #     print(min_sleep, max_sleep, sedentary, moderate, active)
-    #     updated_results = update_row(member_id, cal_consumed, cal_burned, min_sleep, max_sleep, sedentary, moderate, active)
+
     app.run(port=8080)
